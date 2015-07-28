5e2f1992665a005882d50ccae8a48c8bcc6ff324
Sapnapatil <sapna.patil1110@gmail.com>
2015-07-28 12:33:11 -0500

    Vigenere Cipher

--- Sapna.Patil.Vigenere/main.py
+++ Sapna.Patil.Vigenere/main.py
@@ -1,27 +1,61 @@
+###############################################
+# Name: Sapna Patil
+# Class: CMPS 5363 Cryptography
+# Date: 28 July 2015
+# Program 2 - Randomized Vigenère Cipher
+###############################################
+#---------------------Directories-------------------------------------------------------------------
+#It imports directories those are required in function
 import argparse
-import sys
 import randomized_vigenere as rv
-
+#-------------Main Function-------------------------------------------------------------------------
+#starting point of program is an main function
 def main():
+    #The argparse module makes it easy to write user-friendly command-line interfaces
     parser = argparse.ArgumentParser()
-
+    #Filling an ArgumentParser with information about program arguments is done by making calls to the add_argument() method.
     parser.add_argument("-m", "--mode", dest="mode", default = "encrypt", help="Encrypt or Decrypt")
     parser.add_argument("-i", "--inputfile", dest="inputFile", help="Input Name")
     parser.add_argument("-o", "--outputfile", dest="outputFile", help="Output Name")
-    parser.add_argument("-s", "--seed", dest="seed",help="Integer seed")
-
+    parser.add_argument("-s", "--seed", dest="seed", help="Integer seed")
+    #calling parse_args() will return an object
     args = parser.parse_args()
-
-    #f = open(args.inputFile,'r')
-    f = open(Inputfile.txt,'r')
-    message = f.read()
-    if(args.type == 'add'):
-        data = rv.encrypt(message,args.mode,args.seed)
+    
+    #to check whether mode is encrypt
+    if(args.mode == 'encrypt'):
+        #to get seed from args parameter
+        seed = args.seed
+        seed=int(seed)
+        print("the seed value is ",seed)
+        #following fuction is used to open file and read message
+        f = open(args.inputFile,'r')
+        message = f.read()
+        #call function from other file to pass the parameter using object of that file
+        Vigmat=rv.encrypt(seed,message,'encrypt')
+        #below function is used to write output to file
+        print("Encrypted Message is:",Vigmat)
+        o = open(args.outputFile,'w')
+        o.write(str(Vigmat))
+        
+        #below else part wil be decrypt function
     else:
-        data = rv.decrypt(message,args.mode,args.seed)
-    o = open(args.outputFile,'w')
-    o.write(str(data))
-
-
+        seed = args.seed
+        seed=int(seed)
+        f = open(args.inputFile,'r')
+        message = f.read()
+        Vigmat=rv.decrypt(seed,message,'decrypt')
+        #below function is used to write output to file
+        print("Decrypted Message is:",Vigmat)
+        o = open(args.outputFile,'w')
+        o.write(str(Vigmat))
+#----------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     main()
+#----------------------------------------------------------------------------------------------------------------
+
+        
+        
+        
+        
+    
+    
--- Sapna.Patil.Vigenere/randomized_vigenere.py
+++ Sapna.Patil.Vigenere/randomized_vigenere.py
@@ -1,42 +1,125 @@
+#----------------------------------------------------------------------------------------------------------
+###############################################
+# Name: Sapna Patil
+# Class: CMPS 5363 Cryptography
+# Date: 28 July 2015
+# Program 2 - Randomized Vigenère Cipher
+###############################################
 import random
-
-#############################################################################
-# keywordFromSeed -
-#    Works by peeling off two digits at a time, and using modulo to map it into
-#    the proper range of A-Z for use as a keyword.
-# Example:
-#    This example spells math, and I chose values 0-25 on purpose, but
-#    it really doesn't matter what values we choose because 99 % 26 = 21 or 'V' 
-#    or any value % 26 for that matter.
-#
-#    seed = 12001907
-#    l1   = 12001907 % 100 = 07 = H
-#    seed = 12001907 // 100 = 120019
-#    l2   = 120019 % 100 = 19 = T
-#    seed = 120019 // 100 = 1200
-#    l3   = 1200 % 100 = 0 = A
-#    seed = 1200 // 100 = 12
-#    l4   = 12 % 100 = 12 = M
-#    seed = 12 // 100 = 0
-#
-# @param {int} seed - An integer value used to seed the random number generator
-#                     that we will use as our keyword for vigenere
-# @return {string} keyword - a string representation of the integer seed
-#############################################################################
+#----------------------------------------------------------------------------------------------------------
+#Symbol string shows all 95 characters
+symbols= """ !"#$%&'()*+,-./0123456789:;?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+vigenere = [[0 for i in range(len(symbols))] for i in range(len(symbols))]
+#----------------------------------------------------------------------------------------------------------       
+#----------------------------------------------------------------------------------------------------------
+#Togetfunction is used to get random seed
+def togetseed(seed):
+	return random.seed(seed)
+#----------------------------------------------------------------------------------------------------------    
+    
+	
+#----------------------------------------------------------------------------------------------------------	
+#setmessage is used to setmessage
+def setMessage(message):
+	return message
+#----------------------------------------------------------------------------------------------------------
+#Below function is used to Generate keyword from seed		
 def keywordFromSeed(seed):
-    Letters = []
+	Letters = []
+	#seed=int(seed)
+	while seed > 0:
+		Letters.insert(0,chr((seed % 100) % 26 + 65))
+		seed = seed // 100
+		keyWord = "".join(Letters)
+	return keyWord
+#----------------------------------------------------------------------------------------------------------
+#This function is used to generate vigenere matrix using 95*95 combination of Symbols
+def buildVigenere(symbols,seed):
+    #generate the seed using random function 
+    random.seed(seed)
+    #to get length of symbol 
+    n = len(symbols)
+    vigenere = [[0 for i in range(n)] for i in range(n)]
+    symbols = list(symbols)
+    random.shuffle(symbols)
+    symbols = ''.join(symbols)
+    print('new symbols:')
+    print(symbols)
+    print(' ')
+    
+    for sym in symbols:
+        random.seed(seed)
+        myList = []
+    
+        for i in range(n):
+            r = random.randrange(n)
+            
+            if r not in myList:
+                myList.append(r)
+            else:
+                while(r in myList):
+                    r = random.randrange(n)
+            
+                myList.append(r)
+                               
+            while(vigenere[i][r] != 0):
+                r = (r + 1) % n
+            
+            vigenere[i][r] = sym
+            
+    return vigenere
+#----------------------------------------------------------------------------------------------------------
 
-    while seed > 0:
-        Letters.insert(0,chr((seed % 100) % 26 + 65))
-        seed = seed // 100
-    return ''.join(Letters)
-
-#Usage:
-
-seed = 12001907
-
-random.seed(12001907)
-
-keyWord = keywordFromSeed(seed)
-
-print(keyWord)   # Prints "MATH"
+#---------------------------------------------------------------------------------------------------------- 
+#To encrypt plainmessage i need to have seed and message by using seed i can generate the keyword and using
+# keyword and the message i am getting through that it easy to encrypt message
+#  
+def encrypt(seed, message,mode):
+         message=setMessage(message)
+         ciphertext = ""
+         vigenere=buildVigenere(symbols,seed)
+         #print('seed value in rv',seed)
+         keyWord=keywordFromSeed(seed)
+         if(mode== 'encrypt'):
+            for i in range(len(message)):
+                mi = i
+                ki = i % len(keyWord)
+                l=len(symbols)
+                row=col=0
+                for r in range(l):
+                    if(message[mi]==vigenere[0][r]):
+                        col=r
+                for c in range(len(symbols)):
+                    if(keyWord[ki]==vigenere[c][0]):
+                        row=c 
+                vigvariable=vigenere[row][col]
+                ciphertext = ciphertext + vigvariable
+                #print(ciphertext)
+         return ciphertext
+#----------------------------------------------------------------------------------------------------------               
+#----------------------------------------------------------------------------------------------------------
+#To decrypt ciphertext i need to have seed and ciphertext by using seed i can generate the keyword and using
+# keyword and the ciphertext i am getting throgh that it easy to decrypt ciphertext to get plaintext
+# 
+def decrypt(seed, message,mode):
+        plaintext = ""
+        message=setMessage(message)
+        vigenere=buildVigenere(symbols,seed)
+        #print('seed value in rv',seed)
+        keyWord=keywordFromSeed(seed)
+        for i in range(len(message)):
+                mi = i
+                ki = i % len(keyWord)
+                n = len(symbols)
+                r=c=0
+                for c in range(len(symbols)):
+                    if(keyWord[ki]==vigenere[c][0]):
+                       row=c
+                for r in range(n):
+                    if(message[mi]==vigenere[row][r]):
+                       col=r
+                decryptChar=vigenere[0][col]   
+                plaintext=plaintext+decryptChar;
+        #print("The plain text is :",plaintext)
+        return plaintext
+#----------------------------------------------------------------------------------------------------------    
